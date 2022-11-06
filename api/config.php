e9d8a758318ad92425c2498c67c84d398703c0e9
99Troniee_ <54875113+xNart99@users.noreply.github.com>
2022-11-07 09:19:41 +1000

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,5 +1,5 @@
 <?php
-	$apikey = 'C6C2E5FD7038EE265CF5B3A881E2BF9C'; //API key, lấy từ website thesieutoc.net thay vào trong cặp dấu ''
+	$apikey = ''; //API key, lấy từ website thesieutoc.net thay vào trong cặp dấu ''
 	// database Mysql config
 	$local_db = "localhost";
 	$user_db = "root";
